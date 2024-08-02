2e49f4bdb789eaaa4099819ae24988838fd32eab
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2024-08-01 21:03:08 -0300

    update

---
+++ PHPprojects/meu-primeiro-projeto-laravel/resources/views/Home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale='1.0'">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+
+<body>
+    @include('components.header')
+    <section id="inicio">
+        <h1>Bem-vindo à nossa Loja de Celulares</h1>
+        <p>Explore os melhores celulares do mercado.</p>
+    </section>
+    <section id="produtos">
+        <h2>Produtos</h2>
+        <p>Aqui você encontrará uma vasta gama de celulares de várias marcas.</p>
+    </section>
+    <section id="contato">
+        <h2>Contato</h2>
+        <p>Entre em contato conosco para mais informações.</p>
+    </section>
+</body>
+
+</html>
