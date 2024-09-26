956001196aa68ce708a5fbfcc9286835f3d5d227
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2024-09-25 21:55:32 -0300

    update

--- Java/colecoes_exemplo/src/App.java
+++ Java/colecoes_exemplo/src/App.java
@@ -1,56 +1,77 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        ListExemplo list = new ListExemplo();
-        System.out.println("EXECUTANDO PROGRAMA LIST EXEMPLO...");
-        System.out.println();
-        list.adicionarNome("Alice");
-        list.adicionarNome("Bob");
-        list.adicionarNome("Carlos");
-        list.adicionarNome("Daniela");
-        list.adicionarNome("Maria");
-        list.adicionarNome("João");
-        list.adicionarNome("Pedro");
-        list.adicionarNome("Ana");
+        // ListExemplo list = new ListExemplo();
+        // System.out.println("EXECUTANDO PROGRAMA LIST EXEMPLO...");
+        // System.out.println();
+        // list.adicionarNome("Alice");
+        // list.adicionarNome("Bob");
+        // list.adicionarNome("Carlos");
+        // list.adicionarNome("Daniela");
+        // list.adicionarNome("Maria");
+        // list.adicionarNome("João");
+        // list.adicionarNome("Pedro");
+        // list.adicionarNome("Ana");
 
-        list.listarNomes();
+        // list.listarNomes();
 
-        list.modificarNome(2, "Beatriz", null);
+        // list.modificarNome(2, "Beatriz", null);
 
-        list.removerNome("João");
+        // list.removerNome("João");
 
-        list.listarNomes();
+        // list.listarNomes();
 
-        list.adicionarNome("Carolina");
-        list.adicionarNome("José");
-        list.adicionarNome("José");
-        list.listarNomes();
-        list.removerNome("José");
+        // list.adicionarNome("Carolina");
+        // list.adicionarNome("José");
+        // list.adicionarNome("José");
+        // list.listarNomes();
+        // list.removerNome("José");
 
-        SetExemplo set = new SetExemplo();
+        // SetExemplo set = new SetExemplo();
+        // System.out.println();
+        // System.out.println("EXECUTANDO PROGRAMA SET EXEMPLO...");
+        // System.out.println();
+        // set.adicionarNome("Alice");
+        // set.adicionarNome("Bob");
+        // set.adicionarNome("Carlos");
+        // set.adicionarNome("Daniela");
+        // set.adicionarNome("Maria");
+        // set.adicionarNome("João");
+        // System.out.println();
+        // set.listarNomes();
+        // System.out.println();
+        // set.removerNome("João");
+        // System.out.println();
+        // set.listarNomes();
+        // System.out.println();
+        // set.adicionarNome("Carolina");
+        // set.adicionarNome("José");
+        // System.out.println();
+        // set.listarNomes();
+        // System.out.println();
+        // set.removerNome("José");
+        // System.out.println();
+        // set.listarNomes();
+        // System.out.println("FIM DO PROGRAMA...");
+        // System.out.println();
+
+        MapExemplo map = new MapExemplo();
+        System.out.println("EXECUTANDO PROGRAMA MAP EXEMPLO...");
         System.out.println();
-        System.out.println("EXECUTANDO PROGRAMA SET EXEMPLO...");
+        map.adicionarNomeIdade("Alice", 18);
+        map.adicionarNomeIdade("Bob", 18);
         System.out.println();
-        set.adicionarNome("Alice");
-        set.adicionarNome("Bob");
-        set.adicionarNome("Carlos");
-        set.adicionarNome("Daniela");
-        set.adicionarNome("Maria");
-        set.adicionarNome("João");
+        map.listarNomesIdades();
         System.out.println();
-        set.listarNomes();
+        map.listarNomesIdades();
         System.out.println();
-        set.removerNome("João");
+        map.adicionarNomeIdade("Carolina", 18);
         System.out.println();
-        set.listarNomes();
+        map.listarNomesIdades();
         System.out.println();
-        set.adicionarNome("Carolina");
-        set.adicionarNome("José");
+        map.removerNome("Bob");
         System.out.println();
-        set.listarNomes();
+        map.listarNomesIdades();
         System.out.println();
-        set.removerNome("José");
-        System.out.println();
-        set.listarNomes();
         System.out.println("FIM DO PROGRAMA...");
         System.out.println();
     }
--- Java/colecoes_exemplo/src/MapExemplo.java
+++ Java/colecoes_exemplo/src/MapExemplo.java
@@ -7,4 +7,30 @@ public class MapExemplo {
     public MapExemplo() {
         nomesIdade = new HashMap<>();
     }
+
+    public void adicionarNomeIdade(String nome, Integer idade) {
+        if (nomesIdade.containsKey(nome)) {
+            System.out.println("Nome já existe: " + nome);
+            return;
+        }
+        nomesIdade.put(nome, idade);
+        System.out.println("Nome adicionado: " + nome + ", Idade: " + idade);
+    }
+
+    public void listarNomesIdades() {
+        System.out.println("Lista de nomes e idades cadastrados:");
+        for (String nome : nomesIdade.keySet()) {
+        System.out.println(" - " + nome + " - " + nomesIdade.get(nome) + " anos");
+        }
+    }
+
+    public void removerNome(String nome) {
+        nomesIdade.remove(nome);
+        System.out.println("Nome removido: " + nome);
+    }
+
+    public void modificarNomeIdade(String nome, Integer idade) {
+        nomesIdade.put(nome, idade);
+        System.out.println("Nome e idade modificados: " + nome + ", " + idade);
+    }
 }
